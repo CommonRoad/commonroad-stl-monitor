fff3080846a5e05bb4d742ec15d52b83481c7bf0
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2020-01-18 16:13:03 +0100

    add exit ramp scenario generation

--- scenarios/test/scenario_generation.py
+++ scenarios/test/scenario_generation.py
@@ -12,6 +12,143 @@ from commonroad.scenario.scenario import Scenario
 from commonroad.scenario.obstacle import DynamicObstacle, ObstacleType
 from commonroad.scenario.trajectory import State, Trajectory
 from common.configuration import *
+import bezier
+
+
+def create_exit_ramp_start(l_id: int) -> Lanelet:
+    right = np.asfortranarray([[10.0, 12.5, 15, 17.5, 19.0, 20.0],
+                               [0.0, -1, -3, -3.25, -3.5, -3.5]])
+    right_curve = bezier.Curve(right, degree=5)
+    ax = right_curve.plot(num_pts=50)
+    x_right, y_right = ax.lines[0].get_data()
+    point_list = []
+    for idx in range(len(x_right)):
+        point = np.array([x_right[idx], y_right[idx]])
+        point_list.append(point)
+    left_vertices = np.array(point_list)
+
+    left = np.asfortranarray([[10.0, 12.5, 15, 17.5, 19.0, 20.0],
+                               [3.5, 2.5, 0.5, 0.25, 0, 0]])
+    left_curve = bezier.Curve(left, degree=5)
+    ax = left_curve.plot(num_pts=50)
+    x_left, y_left = ax.lines[0].get_data()
+    point_list = []
+    for idx in range(len(x_right)):
+        point = np.array([x_left[idx], y_left[idx]])
+        point_list.append(point)
+    right_vertices = np.array(point_list)
+
+    center = np.asfortranarray([[10.0, 12.5, 15, 17.5, 19, 20.0],
+                               [1.75, 0.75, -1.25, -1.5, -1.75, -1.75]])
+    center_curve = bezier.Curve(center, degree=5)
+    ax = center_curve.plot(num_pts=50)
+    x_center, y_center = ax.lines[0].get_data()
+    point_list = []
+    for idx in range(len(x_right)):
+        point = np.array([x_center[idx], y_center[idx]])
+        point_list.append(point)
+    center_vertices = np.array(point_list)
+
+    lanelet = Lanelet(left_vertices=left_vertices, center_vertices=center_vertices,
+                      right_vertices=right_vertices, lanelet_id=l_id, predecessor=[1], successor=[l_id+1],
+                      adjacent_left=None, adjacent_left_same_direction=None,
+                      line_marking_left_vertices=LineMarking.SOLID,
+                      line_marking_right_vertices=LineMarking.SOLID,
+                      lanelet_type= {LaneletType.HIGHWAY, LaneletType.ACCESS_RAMP}, user_one_way={RoadUser.VEHICLE})
+    return lanelet
+
+
+def create_exit_ramp_end(x_start, l_id: int) -> Lanelet:
+    right = np.asfortranarray([[x_start, x_start+2.5, x_start+5, x_start+7.5, x_start+9.0, x_start+10.0],
+                               [-3.5, -3.75, -4.5, -5.5, -6.75, -8.25]])
+    right_curve = bezier.Curve(right, degree=5)
+    ax = right_curve.plot(num_pts=20)
+    x_right, y_right = ax.lines[0].get_data()
+    point_list = []
+    for idx in range(len(x_right)):
+        point = np.array([x_right[idx], y_right[idx]])
+        point_list.append(point)
+    left_vertices = np.array(point_list)
+
+    left = np.asfortranarray([[x_start, x_start+2.5, x_start+5, x_start+7.5, x_start+9.0, x_start+10.0],
+                               [0.0, -0.25, -1.0, -2.0, -3.0, -4.0]])
+    left_curve = bezier.Curve(left, degree=5)
+    ax = left_curve.plot(num_pts=20)
+    x_left, y_left = ax.lines[0].get_data()
+    point_list = []
+    for idx in range(len(x_right)):
+        point = np.array([x_left[idx], y_left[idx]])
+        point_list.append(point)
+    right_vertices = np.array(point_list)
+
+    center = np.asfortranarray([[x_start, x_start+2.5, x_start+5, x_start+7.5, x_start+9.0, x_start+10.0],
+                               [-1.75, -2.0, -2.75, -3.75, -5.0, -6.5]])
+
+    center_curve = bezier.Curve(center, degree=5)
+    ax = center_curve.plot(num_pts=50)
+    x_center, y_center = ax.lines[0].get_data()
+    point_list = []
+    for idx in range(len(x_right)):
+        point = np.array([x_center[idx], y_center[idx]])
+        point_list.append(point)
+    center_vertices = np.array(point_list)
+
+    lanelet = Lanelet(left_vertices=left_vertices, center_vertices=center_vertices,
+                      right_vertices=right_vertices, lanelet_id=l_id, predecessor=[l_id-1], successor=None,
+                      adjacent_left=None, adjacent_left_same_direction=None,
+                      line_marking_left_vertices=LineMarking.SOLID,
+                      line_marking_right_vertices=LineMarking.SOLID,
+                      lanelet_type= {LaneletType.HIGHWAY, LaneletType.ACCESS_RAMP}, user_one_way={RoadUser.VEHICLE})
+    return lanelet
+
+
+def add_exit_ramp(scenario: Scenario, road_length: int, l_id_start: int):
+    lanelet_length = 10
+    num_lanelets_per_lane = road_length // lanelet_length
+    lane_width = 3.5
+
+    # create straight lanelets
+    lanelet = create_exit_ramp_start(l_id_start)
+    scenario.lanelet_network.add_lanelet(lanelet)
+
+    predecessor = [l_id_start]
+    successor = [l_id_start+2]
+    adj_left = 2
+    adj_right = None
+    for lanelet_idx in range(l_id_start+1, l_id_start+num_lanelets_per_lane-2):
+        left_vertices_point_list = []
+        center_vertices_point_list = []
+        right_vertices_point_list = []
+
+        for i in range(lanelet_length + 1):
+            left_vertices_point_list.append(np.array([i + lanelet_length * (lanelet_idx - l_id_start + 1), 0]))
+            center_vertices_point_list.append(np.array([i + lanelet_length *
+                                                        (lanelet_idx - l_id_start + 1),  -0.5 * lane_width]))
+            right_vertices_point_list.append(np.array([i + lanelet_length *
+                                                       (lanelet_idx - l_id_start + 1), -lane_width]))
+
+        left_vertices = np.array(left_vertices_point_list)
+        center_vertices = np.array(center_vertices_point_list)
+        right_vertices = np.array(right_vertices_point_list)
+
+        lanelet = Lanelet(left_vertices=left_vertices, center_vertices=center_vertices,
+                          right_vertices=right_vertices, lanelet_id=lanelet_idx, predecessor=predecessor,
+                          successor=successor, adjacent_left=adj_left, adjacent_left_same_direction=True,
+                          adjacent_right=adj_right, adjacent_right_same_direction=False,
+                          line_marking_left_vertices=LineMarking.SOLID,
+                          line_marking_right_vertices=LineMarking.SOLID,
+                          lanelet_type={LaneletType.HIGHWAY, LaneletType.ACCESS_RAMP}, user_one_way={RoadUser.VEHICLE})
+        scenario.lanelet_network.add_lanelet(lanelet)
+        adj_left += 1
+        predecessor = [lanelet_idx]
+        if lanelet_idx  % num_lanelets_per_lane != 0.0:
+            successor = [lanelet_idx + 1]
+        else:
+            successor = None
+    lanelet = create_exit_ramp_end(road_length-lanelet_length, l_id_start+num_lanelets_per_lane-2)
+    scenario.lanelet_network.add_lanelet(lanelet)
+
+    return scenario
 
 
 def create_straight_scenario(commonroad_benchmark_id: str, dt: float, num_straight_lanes: int, num_lanelets_per_lane: int,
@@ -139,22 +276,22 @@ def create_max_speed_limit_scenario():
                                             1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                                             1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                                             1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
-                                            -5, -5, -5, -5, -5, -5, -5, -5, -5, -5], 34, np.array([3.0, 1.75]), 100)
+                                            -5, -5, -5, -5, -5, -5, -5, -5, -5, -5], 34, np.array([3.0, 1.75]), 1000)
     obs2 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 35, np.array([10.0, 5.25]), 101)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 35, np.array([10.0, 5.25]), 1001)
     obs3 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0 ,0 ,0 ,0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 36, np.array([3.0, 8.75]), 102)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 36, np.array([3.0, 8.75]), 1002)
     obs4 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 30, np.array([3.0, 5.25]), 103)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 30, np.array([3.0, 5.25]), 1003)
     obstacles.append(obs1)
     obstacles.append(obs2)
     obstacles.append(obs3)
@@ -173,47 +310,47 @@ def create_min_speed_limit_scenario():
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 5, np.array([3.0, 1.75]), 100)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 5, np.array([3.0, 1.75]), 1000)
     obs2 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([13.0, 1.75]), 101)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([13.0, 1.75]), 1001)
     obs3 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 10, np.array([3.0, 5.25]), 102)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 10, np.array([3.0, 5.25]), 1002)
     obs4 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 15, np.array([13.0, 5.25]), 103)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 15, np.array([13.0, 5.25]), 1003)
     obs5 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 25, np.array([22.0, 5.25]), 104)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 25, np.array([22.0, 5.25]), 1004)
     obs6 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 22, np.array([3.0, 8.75]), 105)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 22, np.array([3.0, 8.75]), 1005)
     obs7 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 22, np.array([13.0, 8.75]), 106)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 22, np.array([13.0, 8.75]), 1006)
     obs8 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 22, np.array([3.0, 12.25]), 107)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 22, np.array([3.0, 12.25]), 1007)
     obs9 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 35, np.array([13.0, 12.25]), 108)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 35, np.array([13.0, 12.25]), 1008)
 
     obstacles.append(obs1)
     obstacles.append(obs2)
@@ -241,37 +378,37 @@ def create_safe_distance_scenario():
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 40, np.array([3.0, 1.75]), 100)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 40, np.array([3.0, 1.75]), 1000)
     obs2 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([125.0, 1.75]), 101)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([125.0, 1.75]), 1001)
     obs3 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 30, np.array([3.0, 5.25]), 102)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 30, np.array([3.0, 5.25]), 1002)
     obs4 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 30, np.array([13.0, 5.25]), 103)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 30, np.array([13.0, 5.25]), 1003)
     obs5 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([75.0, 5.25]), 104)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([75.0, 5.25]), 1004)
     obs6 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([3.0, 8.75]), 105)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([3.0, 8.75]), 1005)
     obs7 = create_obstacle_by_acceleration([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([40.0, 8.75]), 106)
+                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20, np.array([40.0, 8.75]), 1006)
     obstacles.append(obs1)
     obstacles.append(obs2)
     obstacles.append(obs3)
@@ -279,10 +416,15 @@ def create_safe_distance_scenario():
     obstacles.append(obs5)
     obstacles.append(obs6)
     obstacles.append(obs7)
-    scenario = create_straight_scenario("test_safe_distance", 0.1, 3, 5, 250, obstacles)
+    num_lanes = 3
+    num_lanelets = 25
+    road_length = 250
+    scenario = create_straight_scenario("test_safe_distance", 0.1, num_lanes, num_lanelets, road_length, obstacles)
     traffic_sign_elem = TrafficSignElement(TrafficSignIDGermany.MAXSPEED.value, [str(22.22)])
     traffic_sign = TrafficSign(201, [traffic_sign_elem])
     scenario.lanelet_network.add_traffic_sign(traffic_sign, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})
+    add_exit_ramp(scenario, road_length, num_lanes * num_lanelets + 1)
+
     return scenario
 
 
@@ -290,17 +432,17 @@ def main():
     config = load_yaml("./../../config.yaml")
     visualization_param = config.get("visualization").get("video")
 
-    scenario = create_max_speed_limit_scenario()
-    write_to_file(scenario)
-    create_scenario_video("./../../videos", scenario, visualization_param, 50)
+    #scenario = create_max_speed_limit_scenario()
+    #write_to_file(scenario)
+    #create_scenario_video("./../../videos", scenario, visualization_param, 50)
 
-    scenario = create_min_speed_limit_scenario()
-    write_to_file(scenario)
-    create_scenario_video("./../../videos", scenario, visualization_param, 50)
+    #scenario = create_min_speed_limit_scenario()
+    #write_to_file(scenario)
+    #create_scenario_video("./../../videos", scenario, visualization_param, 50)
 
     scenario = create_safe_distance_scenario()
     write_to_file(scenario)
-    create_scenario_video("./../../videos", scenario, visualization_param, 50)
+    #create_scenario_video("./../../videos", scenario, visualization_param, 50)
 
 
 if __name__ == "__main__":
