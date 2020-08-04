Fix some robustness calculation and corresponding tests

test/test_braking_predicates.py
@@ -193,12 +193,13 @@ class TestBrakingPredicates(unittest.TestCase):
         exp_sol_constraint_mode_4 = -1.5 + self._traffic_rule_param["a_abrupt"]
         exp_sol_constraint_mode_5 = self._traffic_rule_param["a_abrupt"]
         exp_sol_constraint_mode_6 = self._traffic_rule_param["a_abrupt"]
-        exp_sol_robustness_mode_1 = -self._traffic_rule_param["a_abrupt"]
-        exp_sol_robustness_mode_2 = -4 - self._traffic_rule_param["a_abrupt"]
-        exp_sol_robustness_mode_3 = -5 - self._traffic_rule_param["a_abrupt"]
-        exp_sol_robustness_mode_4 = 2.5 + self._traffic_rule_param["a_abrupt"]
-        exp_sol_robustness_mode_5 = -4 + self._traffic_rule_param["a_abrupt"]
-        exp_sol_robustness_mode_6 = 2 - self._traffic_rule_param["a_abrupt"]
+
+        exp_sol_robustness_mode_1 = self._traffic_rule_param["a_abrupt"]
+        exp_sol_robustness_mode_2 = 4 + self._traffic_rule_param["a_abrupt"]
+        exp_sol_robustness_mode_3 = 5 + self._traffic_rule_param["a_abrupt"]
+        exp_sol_robustness_mode_4 = 1.5 + self._traffic_rule_param["a_abrupt"]
+        exp_sol_robustness_mode_5 = 8 + self._traffic_rule_param["a_abrupt"]
+        exp_sol_robustness_mode_6 = 2
 
         right_vertices = np.array([[0, 0], [10, 0], [20, 0], [30, 0], [40, 0], [50, 0], [60, 0], [70, 0], [80, 0]])
         left_vertices = np.array([[0, 4], [10, 4], [20, 4], [30, 4], [40, 4], [50, 4], [60, 4], [70, 4], [80, 4]])