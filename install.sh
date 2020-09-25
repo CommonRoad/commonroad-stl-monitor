b37eea35a7df8ca6abd19cb73f3a051f467a619e
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2020-09-25 18:18:12 +0200

    update ci

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-- echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-- apt-get update -y > /dev/null
-- apt-get install -y apt-utils > /dev/null
-- apt-get install -y build-essential > /dev/null
-- apt-get install -y pkg-config > /dev/null
-- apt-get install -y libeigen3-dev > /dev/null
-- apt-get install -y libboost-all-dev
-- apt-get install -y cmake > /dev/null
+#!/bin/bash
+
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+apt-get update -y > /dev/null
+apt-get install -y apt-utils > /dev/null
+apt-get install -y build-essential > /dev/null
+apt-get install -y pkg-config > /dev/null
+apt-get install -y libeigen3-dev > /dev/null
+apt-get install -y libboost-all-dev
+apt-get install -y cmake > /dev/null
