f82e9b2303191a1d309f71bfa6034cdf51a99348
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2020-09-26 10:46:40 +0200

    change directory of driveability checker in ci

--- ci/install_cps_software.sh
+++ ci/install_cps_software.sh
@@ -2,7 +2,7 @@
 git config --global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.lrz.de/".insteadOf "git@gitlab.lrz.de:"
 
 # drivability-checker
-git clone https://gitlab.lrz.de/tum-cps/commonroad-drivability-checker.git
+git clone https://gitlab.lrz.de/tum-cps/commonroad-drivability-checker.git "$HOME"/commonroad-drivability-checker
 cd commonroad-drivability-checker || exit
 bash build.sh -e "/opt/conda/envs/commonroad/" -v 3.6 --cgal --serializer --no-root -j 4
 
