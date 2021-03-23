fa6db4bc61e9dcf89e0a8c7a6e7c5e5612612619
Luis Gressenbuch <luis.gressenbuch@tum.de>
2021-03-23 15:21:12 +0100

    Remove unused predicates

--- crmonitor/predicates/python/predicate.py
+++ crmonitor/predicates/python/predicate.py
@@ -1,7 +1,6 @@
 import abc
 import logging
 import math
-from functools import partial
 from typing import List, Tuple, Set
 
 import numpy as np
@@ -62,6 +61,7 @@ def get_preceding_vehicles(world_state: WorldState, vehicle_rear: Vehicle) -> \
                 veh.append((dist, vehicle_lead))
     return sorted(veh, key=lambda d: d[0])
 
+
 def get_succeeding_vehicles(world_state: WorldState, vehicle_front: Vehicle) -> \
         List[Tuple[float, Vehicle]]:
     """
@@ -86,18 +86,6 @@ def get_succeeding_vehicles(world_state: WorldState, vehicle_front: Vehicle) -> 
             if dist >= 0.0:
                 veh.append((dist, vehicle_rear))
     return sorted(veh, key=lambda d: d[0])
-
-
-class LazyValue:
-    def __init__(self, function):
-        self._function = function
-        self._value = None
-
-    @property
-    def value(self):
-        if self._value is None:
-            self._value = self._function()
-        return self._value
 
 
 class IPredicateEvaluator(abc.ABC):
@@ -131,13 +119,6 @@ class IPredicateEvaluator(abc.ABC):
         # TODO: Might be slow
         # angle = math.asin(math.sin(x))
         return self._scale(x, 0, math.pi, copysign=True)
-
-    def evaluate_boolean_lazy(self, world_state: WorldState,
-                              vehicle_ids: List[int]):
-        fun = partial(self.evaluate_boolean, world_state=world_state,
-                      vehicles=vehicle_ids)
-        lazy_value = LazyValue(fun)
-        return lazy_value
 
     def evaluate_boolean(self, world_state: WorldState,
                          vehicle_ids: List[int]) -> bool:
@@ -344,55 +325,6 @@ class PredSafeDistPrec(IPredicateEvaluator):
         rob = self._scale_lon_dist(delta_s - safe_distance)
         return rob
 
-
-class PredUnnecessaryBraking(IPredicateEvaluator):
-    predicate_name = "unnecessary_braking"
-    arity = 1
-
-    def __init__(self, config):
-        super().__init__(config)
-        self._same_lane_evaluator = PredInSameLane(config)
-        self._front_evaluator = PredInFrontOf(config)
-        self._safe_distance_evaluator = PredSafeDistPrec(config)
-        self.a_abrupt = config["a_abrupt"]
-
-    def evaluate_robustness(self, world_state: WorldState,
-                            vehicle_ids: List[int]) -> float:
-        other_ids = [veh.id for veh in world_state.other_vehicles] + [
-            world_state.ego_vehicle.id]
-        other_ids.remove(vehicle_ids[0])
-        ego_acc = world_state.vehicle_by_id(vehicle_ids[0]).states_lon[
-            world_state.time_step].a
-        # # Short circuit: If we are not breaking, no unnecessary braking
-        # if ego_acc >= 0.0:
-        #     return -ego_acc + 0.0
-        excemption_a = [math.inf]
-        excemption_b = [-math.inf]
-        for o_id in other_ids:
-            if not world_state.vehicle_by_id(o_id).is_valid(
-                    world_state.time_step):
-                continue
-            ids = [vehicle_ids[0], o_id]
-            same_lane = self._same_lane_evaluator.evaluate_robustness(
-                    world_state, ids)
-            front_of = self._front_evaluator.evaluate_robustness(world_state,
-                                                                 ids)
-            safe_dist = self._safe_distance_evaluator.evaluate_robustness(
-                    world_state, ids)
-            excemption_a.append(-min(front_of, same_lane))
-            other_acc = world_state.vehicle_by_id(o_id).states_lon[
-                world_state.time_step].a
-            acc_diff = self._scale_acc(self.a_abrupt + other_acc - ego_acc)
-            excemption_b.append(min(safe_dist, front_of, same_lane, acc_diff))
-
-        min_excempt_a = min(min(excemption_a),
-                            self._scale_acc(self.a_abrupt - ego_acc))
-        max_excempt_b = max(excemption_b)
-
-        rob = min(self._scale_acc(-ego_acc), max(min_excempt_a, max_excempt_b))
-        return rob
-
-
 class PredGenericSpeedLimit(IPredicateEvaluator):
     def __init__(self, config: CommentedMap):
         super().__init__(config)
@@ -451,6 +383,7 @@ class PredFovSpeedLimit(PredGenericSpeedLimit):
         vehicle = world_state.vehicle_by_id(vehicle_ids[0])
         return vehicle.vehicle_param.get("fov_speed_limit")
 
+
 class PredBrSpeedLimit(PredGenericSpeedLimit):
     predicate_name = "keeps_brake_speed_limit"
     arity = 1
@@ -470,38 +403,6 @@ class PredLaneSpeedLimitStar(PredLaneSpeedLimit):
         if speed_limit is None:
             speed_limit = 130.0 / 3.6
         return speed_limit
-
-
-class PredLeadingVehicle(IPredicateEvaluator):
-    predicate_name = "has_leading_vehicle"
-    arity = 1
-
-    def __init__(self, config: CommentedMap):
-        super().__init__(config)
-        self._same_lane_evaluator = PredInSameLane(config)
-        self._front_evaluator = PredInFrontOf(config)
-
-    def evaluate_robustness(self, world_state: WorldState,
-                            vehicle_ids: List[int]) -> float:
-        other_ids = [veh.id for veh in world_state.other_vehicles] + [
-            world_state.ego_vehicle.id]
-        other_ids.remove(vehicle_ids[0])
-        rob_values = []
-        for o_id in other_ids:
-            if not world_state.vehicle_by_id(o_id).is_valid(
-                    world_state.time_step):
-                continue
-            ids = [vehicle_ids[0], o_id]
-            same_lane = self._same_lane_evaluator.evaluate_robustness(
-                    world_state, ids)
-            front_of = self._front_evaluator.evaluate_robustness(world_state,
-                                                                 ids)
-            rob_values.append(min(same_lane, front_of))
-        if len(rob_values) == 0:
-            rob = self._scale_lon_dist(-math.inf)
-        else:
-            rob = min(rob_values)
-        return rob
 
 
 class PredPrecedes(IPredicateEvaluator):
