ba1a7f5f426e39f4632455f87c9473fcc17886fc
Luis Gressenbuch <luis.gressenbuch@tum.de>
2022-03-17 18:56:16 +0100

    Try to fix ci test

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
         "numba==0.51.2",
         "commonroad-io==2021.1",
         "commonroad-vehicle-models==1.0.0",
-        "ruamel-yaml==0.16.12"
+        "ruamel-yaml==0.16.12",
+        "antlr4-python3-runtime==4.5"
     ],  # package_dir={"": "src"},
     data_files=[('crmonitor', ['*.yaml'])],
 )
