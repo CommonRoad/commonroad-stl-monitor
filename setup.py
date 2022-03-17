e2067529910f724591c87dc469f8014545eeb466
Luis Gressenbuch <luis.gressenbuch@tum.de>
2022-03-17 14:23:31 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="stl-crmonitor",
+    packages=find_packages(
+        exclude=[
+            "external",
+            "scenarios",
+            "tools",
+        ]
+    ),
+    install_requires=[
+        "antlr4-python3-runtime==4.5",
+        "rtamt==0.2.5",
+        "numba==0.51.2",
+        "commonroad-io==2021.1",
+        "commonroad-vehicle-models==1.0.0",
+        "ruamel-yaml==0.16.12"
+    ],  # package_dir={"": "src"},
+)
