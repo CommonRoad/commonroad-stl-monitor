c1ffd4cb28c677a608a2c1dd02bf5f4a14419f51
Luis Gressenbuch <luis.gressenbuch@tum.de>
2022-06-23 14:10:47 +0200

    Remove time step from world state

--- crmonitor/common/world_state.py
+++ crmonitor/common/world_state.py
@@ -1,36 +1,40 @@
-import dataclasses
+import logging
+import importlib.resources as pkg_resources
 import logging
 import shelve
 from collections import defaultdict
 from dataclasses import dataclass
 from functools import partial
 from pathlib import Path
-from typing import Optional, Set, Dict
+from typing import Optional, Set
 
-import crmonitor
 import numpy as np
 from commonroad.scenario.scenario import Scenario
-from ruamel.yaml import YAML
 
-from crmonitor.common.helper import (create_scenario_vehicles, create_ego_vehicle_param, create_simulation_param,
-                                     create_other_vehicles_param, load_yaml, )
+import crmonitor
+from crmonitor.common.helper import (
+    create_other_vehicles_param,
+    load_yaml,
+)
 from crmonitor.common.road_network import RoadNetwork
-from crmonitor.common.vehicle import Vehicle, DynamicObstacleVehicle, CurvilinearStateManager, ControlledVehicle, \
-    PredicateCache
-import importlib.resources as pkg_resources
+from crmonitor.common.vehicle import (
+    Vehicle,
+    DynamicObstacleVehicle,
+    CurvilinearStateManager,
+    PredicateCache,
+)
+
 
 @dataclass
 class WorldState:
     vehicles: Set[Vehicle]
     road_network: RoadNetwork
-    time_step: int = 0
     scenario: Optional[Scenario] = None
     cache: Optional[shelve.Shelf] = None
 
-
     @classmethod
     def create_from_scenario(
-        cls, scenario: Scenario, config=None, time_step=0, road_network=None, cache_dir="/tmp"
+        cls, scenario: Scenario, config=None, road_network=None, cache_dir="/tmp"
     ):
         if config is None:
             with pkg_resources.path(crmonitor, "config.yaml") as config_path:
@@ -46,13 +50,30 @@ class WorldState:
         cache = shelve.open(str(cache_file), writeback=True)
         for obs in scenario.dynamic_obstacles:
             cls.augment_state_acceleration_jerk(scenario.dt, obs)
-            curvi_cache, predicate_dict = cache.setdefault(str(obs.obstacle_id), (dict(), defaultdict(partial(defaultdict, dict))))
-            vehicles.add(DynamicObstacleVehicle(obs, CurvilinearStateManager(road_network, curvi_cache), others_params, PredicateCache(predicate_dict)))
-        return cls(vehicles, road_network, time_step, scenario, cache)
+            curvi_cache, predicate_dict = cache.setdefault(
+                str(obs.obstacle_id), (dict(), defaultdict(partial(defaultdict, dict)))
+            )
+            vehicles.add(
+                DynamicObstacleVehicle(
+                    obs,
+                    CurvilinearStateManager(road_network, curvi_cache),
+                    others_params,
+                    PredicateCache(predicate_dict),
+                )
+            )
+        return cls(vehicles, road_network, scenario, cache)
 
     @staticmethod
     def augment_state_acceleration_jerk(dt, obs):
-        accelerations = (np.diff([s.velocity for s in [obs.initial_state] + obs.prediction.trajectory.state_list]) / dt).tolist()
+        accelerations = (
+            np.diff(
+                [
+                    s.velocity
+                    for s in [obs.initial_state] + obs.prediction.trajectory.state_list
+                ]
+            )
+            / dt
+        ).tolist()
         obs.initial_state.acceleration = accelerations[0]
         if len(accelerations) >= 2:
             jerk = (np.diff(accelerations) / dt).tolist()
@@ -61,13 +82,12 @@ class WorldState:
             obs.initial_state.jerk = jerk[0]
         else:
             jerk = [None] * 2
-        for a, j, state in zip(accelerations[1:], jerk[1:], obs.prediction.trajectory.state_list):
+        for a, j, state in zip(
+            accelerations[1:], jerk[1:], obs.prediction.trajectory.state_list
+        ):
             state.acceleration = a
             if j is not None:
                 state.jerk = j
-
-    def step(self):
-        self.time_step += 1
 
     def vehicle_by_id(self, id) -> Optional[Vehicle]:
         for veh in self.vehicles:
@@ -77,14 +97,6 @@ class WorldState:
             logging.warning(f"Vehicle with ID {id} not found!")
             veh = None
         return veh
-
-    # @property
-    # def num_time_steps(self):
-    #     return self._ego_vehicle.state_list_cr[-1].time_step + 1
-
-    # @property
-    # def other_ids(self):
-    #     return [v.id for v in self.other_vehicles]
 
     @property
     def dt(self):
@@ -97,39 +109,3 @@ class WorldState:
         if self.cache is not None:
             logging.info("Cache close!")
             self.cache.close()
-
-    # def __iter__(self):
-    #     return self
-    #
-    # def __next__(self):
-    #     if self.time_step < self.num_time_steps - 1:
-    #         self.step()
-    #         return self
-    #     else:
-    #         raise StopIteration
-
-    # def __eq__(self, o) -> bool:
-    #     if o is None:
-    #         return False
-    #     return (
-    #         self.scenario.scenario_id == o.scenario.scenario_id
-    #         and self.time_step == o.time_step
-    #         and self.ego_vehicle.id == o.ego_vehicle.id
-    #     )
-
-    # def clear_predicate_values_timesteps(self, start_time_step, end_time_step):
-    #     """
-    #     Clear internal cached predicates between for the given time interval
-    #     :param start_time_step: start of the interval (inclusive)
-    #     :param end_time_step: end of the interval (inclusive)
-    #     :return:
-    #     """
-    #     for i in range(start_time_step, end_time_step + 1):
-    #         if i in self.predicate_values:
-    #             self.predicate_values.pop(i)
-    #
-    # def clear_predicates(self):
-    #     self.predicate_values.clear()
-
-    # def copy(self):
-    #     return WorldState(scenario=self.scenario, ego_obs_id=self.ego_vehicle.id, config=self.config, time_step=self.time_step, road_network=self.road_network)
--- crmonitor/evaluation/evaluation.py
+++ crmonitor/evaluation/evaluation.py
@@ -1,39 +1,30 @@
-import copy
+import importlib.resources as pkg_resources
 import logging
-from pathlib import Path
-from typing import List, Iterable, Union, Tuple
+from typing import Tuple, Dict
 
 import numpy as np
 
 import crmonitor
-from ruamel.yaml import YAML
-
 from crmonitor.common.helper import load_yaml
-from crmonitor.common.road_network import RoadNetwork
 from crmonitor.common.vehicle import Vehicle
 from crmonitor.common.world_state import WorldState
 from crmonitor.evaluation.visitor import (MonitorCreationRuleTreeVisitor, EvaluationMonitorTreeVisitor,
                                           PredicateCollectorMonitorTreeVisitor, )
 from crmonitor.monitor.rtamt_monitor_stl import OutputType
 from crmonitor.predicates.rule import VisitorNode, parse_rule
-import importlib.resources as pkg_resources
-
-# def get_valid_time_interval(vehicles: List[Vehicle]):
-#     start = max([v.start_time for v in vehicles])
-#     end = min([v.end_time for v in vehicles])
-#     return start, end
 
 logger = logging.getLogger(__name__)
+
 
 class RuleEvaluator:
 
     @classmethod
     def create_from_config(
-        cls, world_state, ego_vehicle,
+        cls, world_state: WorldState, ego_vehicle: Vehicle,
         rule: str = "R_G1",
         traffic_rules_config=None,
-        use_boolean=False,
-        output_type=OutputType.STANDARD,
+        use_boolean: bool=False,
+        output_type: OutputType=OutputType.STANDARD,
     ):
         if traffic_rules_config is None:
             with pkg_resources.path(crmonitor, "traffic_rules_rtamt.yaml") as traffic_rules_path:
@@ -47,7 +38,7 @@ class RuleEvaluator:
         visitor = MonitorCreationRuleTreeVisitor(world.dt, output_type)
         self._rule = rule
         self._monitor = rule.visit(visitor)
-        self._last_time_step = start_time_step - 1 if start_time_step is not None else ego_vehicle.start_time - 1
+        self._last_evaluation_time_step = start_time_step - 1 if start_time_step is not None else ego_vehicle.start_time - 1
         self._collector_visitor = PredicateCollectorMonitorTreeVisitor()
         self._eval_visitor = EvaluationMonitorTreeVisitor(
             use_boolean=use_boolean, output_type=output_type
@@ -55,7 +46,11 @@ class RuleEvaluator:
         self._ego_vehicle = ego_vehicle
         self._world = world
 
-    def get_predicates(self):
+    @property
+    def current_time(self) -> int:
+        return self._last_evaluation_time_step
+
+    def get_predicates(self) -> Dict[str, float]:
         predicate_values = dict(self._monitor.visit(self._collector_visitor))
         return predicate_values
 
@@ -65,11 +60,12 @@ class RuleEvaluator:
 
         :return: robustness or boolean rule value
         """
-        self._last_time_step += 1
-        if self._ego_vehicle.start_time > self._last_time_step or self._last_time_step > self._ego_vehicle.end_time:
+        self._last_evaluation_time_step += 1
+        if self._ego_vehicle.start_time > self._last_evaluation_time_step or self._last_evaluation_time_step > self._ego_vehicle.end_time:
             logger.warning("Evaluating vehicle outside its lifetime!")
             return np.inf
-        assert self._world.time_step == self._last_time_step, f"World time step not as expected! {self._world.time_step} != {self._last_time_step}"
+        # Todo: Remove time step from world state
+        self._world.time_step = self._last_evaluation_time_step
         rule_value = self._eval_visitor.walk(self._monitor, self._world, self._ego_vehicle)
         return rule_value
 
@@ -82,9 +78,8 @@ class RuleEvaluator:
         :return: Array of all rule values for all time steps of the vehicle's known trajectory
         """
         robustness_values = []
-        for i in range(self._last_time_step + 1, self._ego_vehicle.end_time + 1):
+        for i in range(self._last_evaluation_time_step + 1, self._ego_vehicle.end_time + 1):
             robustness_values.append(self.update())
-            self._world.step()
         return np.array(robustness_values)
 
     @property
@@ -93,151 +88,3 @@ class RuleEvaluator:
 
     def reset(self, ego_vehicle=None, other_vehicles=None, road_network=None):
         raise NotImplementedError
-
-
-
-# class RuleSetEvaluator:
-#     """
-#     Evaluate individual vehicles of CommonRoad scenarios
-#     """
-#
-#     @classmethod
-#     def create_from_config(
-#         cls,
-#         rules: Union[str, Iterable[str]] = ("R_G1", "R_G2", "R_G3"),
-#         traffic_rules_config=None,
-#         dt=0.1,
-#         use_boolean=False,
-#         output_type=OutputType.STANDARD,
-#     ):
-#         if traffic_rules_config is None:
-#             traffic_rules_config = YAML().load(
-#                 Path(__file__).parent.parent / "traffic_rules_rtamt.yaml"
-#             )
-#         if isinstance(rules, str):
-#             rules = [rules]
-#         rule_str_dict = traffic_rules_config["traffic_rules"]
-#         rule_set = [
-#             parse_rule(rule_str_dict[r], traffic_rules_config, name=r) for r in rules
-#         ]
-#         return cls(rule_set, dt, use_boolean=use_boolean, output_type=output_type)
-#
-#     @classmethod
-#     def create_from_rule_str(
-#         cls,
-#         rule_str: Union[str, Iterable[str], Dict[str, str]],
-#         traffic_rules_config=None,
-#         dt=0.1,
-#         output_type=OutputType.STANDARD,
-#     ):
-#         if traffic_rules_config is None:
-#             traffic_rules_config = YAML().load(
-#                 Path(__file__).parent.parent / "traffic_rules_rtamt.yaml"
-#             )
-#         if isinstance(rule_str, str):
-#             rule_str = {rule_str: rule_str}
-#         if isinstance(rule_str, list):
-#             rule_str = {r: r for r in rule_str}
-#         rule_set = [
-#             parse_rule(r, traffic_rules_config, name=n) for r, n in rule_str.items()
-#         ]
-#         return cls(rule_set, dt, output_type=output_type)
-#
-#     def __init__(
-#         self, rules: Iterable, dt, use_boolean=False, output_type=OutputType.STANDARD
-#     ):
-#         """
-#         :param rules: set of rules to be evaluated
-#         """
-#         self.rules = tuple(rules)
-#         visitor = MonitorCreationRuleTreeVisitor(dt, output_type)
-#         self.monitors = {rule: rule.visit(visitor) for rule in rules}
-#         self._last_world_state = None
-#         self._last_time_step = -1
-#         self.use_boolean = use_boolean
-#         self._collector_visitor = PredicateCollectorMonitorTreeVisitor()
-#         self._eval_visitor = EvaluationMonitorTreeVisitor(
-#             use_boolean=use_boolean, output_type=output_type
-#         )
-#
-#     def reset_monitors(self):
-#         self._last_time_step = -1
-#         for monitor in self.monitors.values():
-#             monitor.reset()
-#
-#     def _check_reset(self, world_state: WorldState):
-#         if (
-#             self._last_world_state is None
-#             or (
-#                 self._last_world_state is not world_state
-#                 and (
-#                     not hasattr(self._last_world_state, "scenario")
-#                     or hasattr(self._last_world_state, "scenario")
-#                     and (
-#                         self._last_world_state.scenario.scenario_id
-#                         != world_state.scenario.scenario_id
-#                         or world_state.ego_vehicle.id
-#                         != self._last_world_state.ego_vehicle.id
-#                     )
-#                 )
-#             )
-#             or world_state.time_step < self._last_time_step
-#         ):
-#             logging.debug("Clearing monitor states!")
-#             self._last_world_state = world_state
-#             self._last_time_step = -1
-#             self.reset_monitors()
-#
-#     def evaluate_incremental(
-#         self, world_state: WorldState, to_pandas=True
-#     ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-#         """
-#         Evaluate world state for each time step since the last evaluation.
-#         :param to_pandas: whether convert data to panda dataframe
-#         :param world_state: world state to evaluate
-#         :return: Tuple of pandas dataframes, where the first contains rule
-#             robustness and the second predicate robustness
-#         """
-#         self._check_reset(world_state)
-#         # Only evaluate if ego vehicle is present
-#         time_begin = max(self._last_time_step + 1, world_state.ego_vehicle.start_time)
-#         time_end = min(world_state.time_step, world_state.ego_vehicle.end_time)
-#         self._last_time_step = world_state.time_step
-#         # Create a flat copy of the world to iterate through time
-#         world_state = copy.copy(world_state)
-#         world_state.time_step = time_begin
-#         # Nested dictionary with levels: time step and rule
-#         rule_robustness = {}
-#         # Nested dictionary with levels: time step, rule, predicate name
-#         predicate_robustness = {}
-#         other_ids_values = {}
-#         while world_state.time_step <= time_end:
-#             t = world_state.time_step
-#             rule_robustness[t] = {}
-#             predicate_robustness[t] = {}
-#             other_ids_values[t] = {}
-#             for rule in self.rules:
-#                 rule_robustness[t][rule.name] = self._eval_visitor.walk(
-#                     self.monitors[rule], world_state
-#                 )
-#                 predicate_robustness[t][rule.name] = dict(
-#                     self.monitors[rule].visit(self._collector_visitor)
-#                 )
-#                 other_ids_values[t][rule.name] = self._eval_visitor.other_ids[1:]
-#             world_state.step()
-#
-#         if to_pandas:
-#             df_rule = pandas_from_nested_dict(
-#                 rule_robustness, ["time_step", "rule_name", "robustness"]
-#             )
-#             df_pred = pandas_from_nested_dict(
-#                 predicate_robustness,
-#                 ["time_step", "rule_name", "full_name", "robustness"],
-#             )
-#             df_ids = pandas_from_nested_dict(
-#                 other_ids_values, ["time_step", "rule_name", "other_ids"]
-#             )
-#             df_rule = df_rule.merge(df_ids, on=["time_step", "rule_name"])
-#             return df_rule, df_pred
-#         else:
-#             return rule_robustness, predicate_robustness
