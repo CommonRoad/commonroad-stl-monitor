5c5f65bc8ac92429c8780bdcd8c745ae1686244d
Luis Gressenbuch <luis.gressenbuch@tum.de>
2022-11-09 17:57:24 +0100

    Fix yaml warning

--- crmonitor/common/helper.py
+++ crmonitor/common/helper.py
@@ -7,11 +7,11 @@ from typing import Dict, Union, List, Tuple, Iterable, Sequence
 
 import numba
 import numpy as np
-import ruamel.yaml
 from commonroad.scenario.lanelet import Lanelet, LaneletType
 from commonroad.scenario.obstacle import DynamicObstacle
 from commonroad.scenario.traffic_sign import SupportedTrafficSignCountry
 from commonroad.scenario.trajectory import State
+from ruamel.yaml import YAML
 from vehiclemodels.parameters_vehicle1 import parameters_vehicle1
 from vehiclemodels.parameters_vehicle2 import parameters_vehicle2
 from vehiclemodels.parameters_vehicle3 import parameters_vehicle3
@@ -552,13 +552,8 @@ def load_yaml(file_name: Union[Path, str]) -> Union[Dict, None]:
     :param file_name: name of the yaml file
     """
     file_name = Path(file_name)
-    with file_name.open("r") as stream:
-        try:
-            config = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
-            return config
-        except ruamel.yaml.YAMLError as exc:
-            print(exc)
-            return None
+    config = YAML().load(file_name)
+    return config
 
 
 def update_vehicle(obstacle: DynamicObstacle, dt: float, time_step: int,
