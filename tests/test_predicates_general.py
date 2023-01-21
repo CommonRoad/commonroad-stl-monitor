456a46baf35f88843b5b56a48a8d714806423a97
Mahdi Bayouli <mahdi.bayouli@tum.de>
2023-01-21 16:41:41 +0100

    test_predicates_general

--- tests/test_predicates_general.py
+++ tests/test_predicates_general.py
@@ -349,3 +349,5 @@ class TestGeneralPredicates(unittest.TestCase):
         sol_robustness_monitor_mode_4 = pred.evaluate_robustness(world, 3, vehicle_ids)
         self.assertEqual(exp_sol_monitor_mode_4, sol_monitor_mode_4)
         self.assertEqual(exp_sol_monitor_mode_4, sol_robustness_monitor_mode_4 > 0)
+        
+        
