13603b76210683a75fae800076462afc11b42dba
Janit Grover <janitgvr2@gmail.com>
2023-02-05 10:23:27 +0100

    Debugging Has priority

--- crmonitor/predicates/priority.py
+++ crmonitor/predicates/priority.py
@@ -31,8 +31,8 @@ class PredSamePriority(BasePredicateEvaluator):
         vehicle_k = world.vehicle_by_id(vehicle_ids[0])
         vehicle_p = world.vehicle_by_id(vehicle_ids[1])
 
-        lanelets_dir_ids_of_p = vehicle_p.lanelets_dir(time_step)
-        lanelets_dir_ids_of_k = vehicle_k.lanelets_dir(time_step)
+        lanelets_dir_ids_of_p = vehicle_p.lanelets_dir(time_step, world.road_network)
+        lanelets_dir_ids_of_k = vehicle_k.lanelets_dir(time_step, world.road_network)
 
         priority_p = PredHasPriority.get_priority(lanelets_dir_ids_of_p)
         priority_k = PredHasPriority.get_priority(lanelets_dir_ids_of_k)
@@ -62,8 +62,8 @@ class PredSamePriority(BasePredicateEvaluator):
         vehicle_k = world.vehicle_by_id(vehicle_ids[0])
         vehicle_p = world.vehicle_by_id(vehicle_ids[1])
 
-        lanelets_dir_ids_of_p = vehicle_p.lanelets_dir(time_step, RoadNetwork)
-        lanelets_dir_ids_of_k = vehicle_k.lanelets_dir(time_step, RoadNetwork)
+        lanelets_dir_ids_of_p = vehicle_p.lanelets_dir(time_step)
+        lanelets_dir_ids_of_k = vehicle_k.lanelets_dir(time_step)
 
         priority_p = self.get_priority_dir(lanelets_dir_ids_of_p, vehicle_dir_p)
         priority_k = self.get_priority_dir(lanelets_dir_ids_of_k, vehicle_dir_k)
@@ -196,8 +196,8 @@ class PredHasPriority(BasePredicateEvaluator):
         vehicle_k = world.vehicle_by_id(vehicle_ids[0])
         vehicle_p = world.vehicle_by_id(vehicle_ids[1])
 
-        lanelets_dir_ids_of_p = vehicle_p.lanelets_dir(time_step, RoadNetwork)
-        lanelets_dir_ids_of_k = vehicle_k.lanelets_dir(time_step, RoadNetwork)
+        lanelets_dir_ids_of_p = vehicle_p.lanelets_dir(time_step, world.road_network)
+        lanelets_dir_ids_of_k = vehicle_k.lanelets_dir(time_step, world.road_network)
 
         priority_p = self.get_priority(lanelets_dir_ids_of_p)
         priority_k = self.get_priority(lanelets_dir_ids_of_k)
