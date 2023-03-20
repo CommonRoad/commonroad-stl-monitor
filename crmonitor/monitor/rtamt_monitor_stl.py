b3c3a07438795f987468e4d159ac9eca3da9f861
Luis Gressenbuch <luis.gressenbuch@tum.de>
2023-03-20 18:38:15 +0100

    Use lru cache

--- crmonitor/monitor/rtamt_monitor_stl.py
+++ crmonitor/monitor/rtamt_monitor_stl.py
@@ -1,5 +1,6 @@
 import copy
 from enum import Enum
+from functools import lru_cache
 from typing import List, Tuple
 
 import rtamt
@@ -15,44 +16,40 @@ class OutputType(Enum):
     OUTPUT_ROBUSTNESS = rtamt.Semantics.OUTPUT_ROBUSTNESS
 
 
+@lru_cache(None)
+def construct_monitor(
+    logic_formula: str, output_type: OutputType, predicates, dt
+) -> rtamt.STLSpecification:
+    # Workaround for rtamt when working with output-robustness and input vacuity
+    for pred in predicates:
+        logic_formula = logic_formula.replace(pred[0].name, f"({pred[0].name} >= 0)")
+
+    spec = stl_discrete_time_online_specification_factory(semantics=output_type.value)
+    for var, io_type in predicates:
+        spec.declare_var(var.name, "float")
+        if io_type == IOType.INPUT:
+            spec.set_var_io_type(var.name, "input")
+        else:
+            spec.set_var_io_type(var.name, "output")
+    spec.declare_var("out", "float")
+
+    spec.iosem = output_type
+    spec.unit = "ms"
+    spec.spec = f"out = {logic_formula}"
+    spec.set_sampling_period(dt * 1000.0, "ms")
+    spec.parse()
+    spec.pastify()
+    # new ast of online interpreter is not set until the update method
+    # of AbstractOnlineSpecification is called
+    spec.online_interpreter.set_ast(spec.ast)
+
+    return spec
+
+
 class RtamtStlMonitor:
-    specs = {}
     """
     Represents single formalized STL rule
     """
-
-    @staticmethod
-    def construct_monitor(
-        logic_formula: str, output_type: OutputType, predicates, dt
-    ) -> rtamt.STLSpecification:
-        # Workaround for rtamt when working with output-robustness and input vacuity
-        for pred in predicates:
-            logic_formula = logic_formula.replace(
-                pred[0].name, f"({pred[0].name} >= 0)"
-            )
-
-        spec = stl_discrete_time_online_specification_factory(
-            semantics=output_type.value
-        )
-        for var, io_type in predicates:
-            spec.declare_var(var.name, "float")
-            if io_type == IOType.INPUT:
-                spec.set_var_io_type(var.name, "input")
-            else:
-                spec.set_var_io_type(var.name, "output")
-        spec.declare_var("out", "float")
-
-        spec.iosem = output_type
-        spec.unit = "ms"
-        spec.spec = f"out = {logic_formula}"
-        spec.set_sampling_period(dt * 1000.0, "ms")
-        spec.parse()
-        spec.pastify()
-        # new ast of online interpreter is not set until the update method
-        # of AbstractOnlineSpecification is called
-        spec.online_interpreter.set_ast(spec.ast)
-
-        return spec
 
     @classmethod
     def create_from_rule_node(
@@ -69,13 +66,7 @@ class RtamtStlMonitor:
         self._predicates = predicates
         self._output_type = output_type
         self.dt = dt
-        spec = self.specs.get((rule_str, output_type, dt))
-        if spec is None:
-            spec = self.specs.setdefault(
-                (rule_str, output_type, dt),
-                self.construct_monitor(rule_str, output_type, predicates, dt),
-            )
-            self.specs[(rule_str, output_type, dt)] = spec
+        spec = construct_monitor(rule_str, output_type, tuple(predicates), dt)
         # Flat copy spec and only recreate the online evaluator to
         # avoid parsing the rule.
         self._monitor = spec
