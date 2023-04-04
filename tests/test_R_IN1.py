da48ca07e2e3fe0b12be3764a435b05b4c58ec49
Zekun Xing <zekun.xing@tum.de>
2023-04-04 15:09:42 +0200

    add R_IN1

--- tests/test_R_IN1.py
+++ tests/test_R_IN1.py
@@ -45,37 +45,41 @@ class RuleTest(unittest.TestCase):
 
     def test_R_IN1(self):
         scenario, _ = CommonRoadFileReader(
-                str("../scenarios/test_intersection/DEU_TestStopLineInFront-1_1_T-1.xml")).open(True)
-        dynamic_obstacle_initial_state = CustomState(position=np.array([5, 0.0]), velocity=15, orientation=0.0,
-                                                     time_step=0)
-        state_list_ego = []
-        state_list_ego.append(CustomState(position=np.array([17, 0.0]), velocity=15, orientation=0.0, time_step=1))
-        state_list_ego.append(CustomState(position=np.array([20, 0.0]), velocity=15, orientation=0.0, time_step=2))
-        state_list_ego.append(CustomState(position=np.array([21, 0.0]), velocity=10, orientation=0.0, time_step=3))
-        state_list_ego.append(CustomState(position=np.array([23, 0.0]), velocity=0, orientation=0.0, time_step=4))
-        state_list_ego.append(CustomState(position=np.array([23, 0.0]), velocity=0, orientation=0.0, time_step=5))
-        state_list_ego.append(CustomState(position=np.array([23, 0.0]), velocity=0, orientation=0.0, time_step=6))
-        dynamic_obstacle_trajectory = Trajectory(1, state_list_ego)
-        dynamic_obstacle_shape = Rectangle(width=2, length=5)
-        dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape)
-        dynamic_obstacle_prediction.shape_lanelet_assignment = {0: {1}, 1: {1}, 2: {1, 3}, 3: {1, 3}, 4: {3}, 5: {3}, 6: {3}}
-        dynamic_obstacle_prediction.center_lanelet_assignment = {0: {1}, 1: {1}, 2: {1, 3}, 3: {1, 3}, 4: {3}, 5: {3}, 6: {3}}
-        dynamic_obstacle_id = scenario.generate_object_id()
-        dynamic_obstacle_type = ObstacleType.CAR
-        dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id, dynamic_obstacle_type, dynamic_obstacle_shape,
-                                           dynamic_obstacle_initial_state, dynamic_obstacle_prediction,
-                                           initial_center_lanelet_ids={1}, initial_shape_lanelet_ids={1})
-        scenario.add_objects(dynamic_obstacle)
-        # plt.figure(figsize=(25, 10))
-        # rnd = MPRenderer()
-        # rnd.draw_params.time_begin = 3
-        # scenario.draw(rnd)
-        # rnd.render()
-        # plt.show()
-
+                str("../scenarios/test_intersection/DEU_TestRIN1-2_1_T-1.xml")).open(lanelet_assignment=True)
         world = World.create_from_scenario(scenario)
+
+        # scenario, _ = CommonRoadFileReader(
+        #         str("../scenarios/test_intersection/DEU_TestStopLineInFront-1_1_T-1.xml")).open(True)
+        # dynamic_obstacle_initial_state = CustomState(position=np.array([5, 0.0]), velocity=15, orientation=0.0,
+        #                                              time_step=0)
+        # state_list_ego = []
+        # state_list_ego.append(CustomState(position=np.array([17, 0.0]), velocity=15, orientation=0.0, time_step=1))
+        # state_list_ego.append(CustomState(position=np.array([20, 0.0]), velocity=15, orientation=0.0, time_step=2))
+        # state_list_ego.append(CustomState(position=np.array([21, 0.0]), velocity=10, orientation=0.0, time_step=3))
+        # state_list_ego.append(CustomState(position=np.array([23, 0.0]), velocity=0, orientation=0.0, time_step=4))
+        # state_list_ego.append(CustomState(position=np.array([23, 0.0]), velocity=0, orientation=0.0, time_step=5))
+        # state_list_ego.append(CustomState(position=np.array([23, 0.0]), velocity=0, orientation=0.0, time_step=6))
+        # dynamic_obstacle_trajectory = Trajectory(1, state_list_ego)
+        # dynamic_obstacle_shape = Rectangle(width=2, length=5)
+        # dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape)
+        # dynamic_obstacle_prediction.shape_lanelet_assignment = {0: {1}, 1: {1}, 2: {1, 3}, 3: {1, 3}, 4: {3}, 5: {3}, 6: {3}}
+        # dynamic_obstacle_prediction.center_lanelet_assignment = {0: {1}, 1: {1}, 2: {1, 3}, 3: {1, 3}, 4: {3}, 5: {3}, 6: {3}}
+        # dynamic_obstacle_id = scenario.generate_object_id()
+        # dynamic_obstacle_type = ObstacleType.CAR
+        # dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id, dynamic_obstacle_type, dynamic_obstacle_shape,
+        #                                    dynamic_obstacle_initial_state, dynamic_obstacle_prediction,
+        #                                    initial_center_lanelet_ids={1}, initial_shape_lanelet_ids={1})
+        # scenario.add_objects(dynamic_obstacle)
+        # # plt.figure(figsize=(25, 10))
+        # # rnd = MPRenderer()
+        # # rnd.draw_params.time_begin = 3
+        # # scenario.draw(rnd)
+        # # rnd.render()
+        # # plt.show()
+        #
+        # world = World.create_from_scenario(scenario)
         road_network = RoadNetwork(scenario.lanelet_network, self.config.get("road_network_param"))
-        ego_vehicle = world.vehicle_by_id(dynamic_obstacle_id)
+        ego_vehicle = world.vehicle_by_id(31)
         rule_eval = RuleEvaluator.create_from_config(world, ego_vehicle, "R_IN1")
         rule = rule_eval._rule
         self.assertTrue(isinstance(rule, RuleNode))
--- tests/test_predicate_stop_line_in_front.py
+++ tests/test_predicate_stop_line_in_front.py
@@ -34,50 +34,53 @@ class TestPositionPredicates(unittest.TestCase):
         self.config["d_sl"] = 1.0
 
     def testStopLineInFront(self):
-        scenario, _ = CommonRoadFileReader(
-            str(
-                "../scenarios/test_intersection/DEU_TestStopLineInFront-1_1_T-1.xml"
-            )
-        ).open(True)
-        dynamic_obstacle_initial_state = CustomState(position=np.array([5, 0.0]),
-                                                     velocity=15,
-                                                     orientation=0.0,
-                                                     time_step=0)
-        state_list_ego = []
-        state_list_ego.append(CustomState(position=np.array([17, 0.0]),
-                                          velocity=15,
-                                          orientation=0.0,
-                                          time_step=1))
-        state_list_ego.append(CustomState(position=np.array([23, 0.0]),
-                                          velocity=15,
-                                          orientation=0.0,
-                                          time_step=2))
-        dynamic_obstacle_trajectory = Trajectory(1, state_list_ego)
-        dynamic_obstacle_shape = Rectangle(width=2, length=5)
-        dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape)
-        dynamic_obstacle_prediction.shape_lanelet_assignment = {0: {1}, 1: {1}, 2: {3}}
-        dynamic_obstacle_prediction.center_lanelet_assignment = {0: {1}, 1: {1}, 2: {3}}
-        dynamic_obstacle_id = scenario.generate_object_id()
-        dynamic_obstacle_type = ObstacleType.CAR
-        dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id,
-                                           dynamic_obstacle_type,
-                                           dynamic_obstacle_shape,
-                                           dynamic_obstacle_initial_state,
-                                           dynamic_obstacle_prediction,
-                                           initial_center_lanelet_ids={1},
-                                           initial_shape_lanelet_ids={1})
-        scenario.add_objects(dynamic_obstacle)
-        # plt.figure(figsize=(25, 10))
-        # rnd = MPRenderer()
-        # rnd.draw_params.time_begin = 2
-        # scenario.draw(rnd)
-        # rnd.render()
-        # plt.show()
+        scenario, _ = CommonRoadFileReader(str("../scenarios/test_intersection/DEU_TestRIN1-1_1_T-1.xml")).open(
+            lanelet_assignment=True)
         world = World.create_from_scenario(scenario)
+        # scenario, _ = CommonRoadFileReader(
+        #     str(
+        #         "../scenarios/test_intersection/DEU_TestStopLineInFront-1_1_T-1.xml"
+        #     )
+        # ).open(True)
+        # dynamic_obstacle_initial_state = CustomState(position=np.array([5, 0.0]),
+        #                                              velocity=15,
+        #                                              orientation=0.0,
+        #                                              time_step=0)
+        # state_list_ego = []
+        # state_list_ego.append(CustomState(position=np.array([17, 0.0]),
+        #                                   velocity=15,
+        #                                   orientation=0.0,
+        #                                   time_step=1))
+        # state_list_ego.append(CustomState(position=np.array([23, 0.0]),
+        #                                   velocity=15,
+        #                                   orientation=0.0,
+        #                                   time_step=2))
+        # dynamic_obstacle_trajectory = Trajectory(1, state_list_ego)
+        # dynamic_obstacle_shape = Rectangle(width=2, length=5)
+        # dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape)
+        # dynamic_obstacle_prediction.shape_lanelet_assignment = {0: {1}, 1: {1}, 2: {3}}
+        # dynamic_obstacle_prediction.center_lanelet_assignment = {0: {1}, 1: {1}, 2: {3}}
+        # dynamic_obstacle_id = scenario.generate_object_id()
+        # dynamic_obstacle_type = ObstacleType.CAR
+        # dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id,
+        #                                    dynamic_obstacle_type,
+        #                                    dynamic_obstacle_shape,
+        #                                    dynamic_obstacle_initial_state,
+        #                                    dynamic_obstacle_prediction,
+        #                                    initial_center_lanelet_ids={1},
+        #                                    initial_shape_lanelet_ids={1})
+        # scenario.add_objects(dynamic_obstacle)
+        # # plt.figure(figsize=(25, 10))
+        # # rnd = MPRenderer()
+        # # rnd.draw_params.time_begin = 2
+        # # scenario.draw(rnd)
+        # # rnd.render()
+        # # plt.show()
+        # world = World.create_from_scenario(scenario)
         road_network = RoadNetwork(scenario.lanelet_network, self.config.get("road_network_param"))
-        ego_vehicle = world.vehicle_by_id(dynamic_obstacle_id)
+        ego_vehicle = world.vehicle_by_id(31)
 
-        for time in range(ego_vehicle.end_time + 1):
+        for time in range(20, ego_vehicle.end_time + 1):
             pred = PredStopLineInFront(self.config)
             sol_monitor_1 = pred.evaluate_boolean(world, time, [ego_vehicle.id])
             print(sol_monitor_1)
