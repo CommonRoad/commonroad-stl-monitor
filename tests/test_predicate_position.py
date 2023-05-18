194c0cd9c45e11faaa4bdd14a32539aaef7bdcd4
Zekun Xing <zekun.xing@tum.de>
2023-05-18 18:33:56 +0200

    test position predicates

--- tests/test_predicate_position.py
+++ tests/test_predicate_position.py
@@ -55,12 +55,13 @@ class TestPositionPredicates(unittest.TestCase):
             print(sol_monitor_2)
 
     def testOnIncomingLeftOf(self):
-        scenario, _ = CommonRoadFileReader(str("../scenarios/test_intersection/DEU_test_traffic_signs_1.xml")).open(
+        scenario, _ = CommonRoadFileReader(
+                str("../scenarios/test_intersection/DEU_TestIntersectionInteract-2_1_T-1.xml")).open(
                 lanelet_assignment=True)
         world = World.create_from_scenario(scenario)
         road_network = RoadNetwork(scenario.lanelet_network, self.config.get("road_network_param"))
-        ego_vehicle = world.vehicle_by_id(1002)
-        target_vehicle = world.vehicle_by_id(1001)
+        ego_vehicle = world.vehicle_by_id(30)
+        target_vehicle = world.vehicle_by_id(31)
         for time in range(min(ego_vehicle.end_time, target_vehicle.end_time) + 1):
             pred = PredOnIncomingLeftOf(self.config)
             sol_monitor_1 = pred.evaluate_boolean(world, time, [ego_vehicle.id, target_vehicle.id])
@@ -70,19 +71,41 @@ class TestPositionPredicates(unittest.TestCase):
             print(sol_monitor_2)
 
     def testInIntersectionConflictArea(self):
-        scenario, _ = CommonRoadFileReader(str("../scenarios/test_intersection/DEU_TestIntersectionInteract-1_1_T-1.xml")).open(
+        # scenario, _ = CommonRoadFileReader(str("../scenarios/test_intersection/DEU_TestIntersectionInteract-1_1_T-1.xml")).open(
+        #         lanelet_assignment=True)
+        scenario, _ = CommonRoadFileReader(
+            str("../scenarios/test_intersection/DEU_TestIntersectionInteract-2_1_T-1.xml")).open(
                 lanelet_assignment=True)
         world = World.create_from_scenario(scenario)
         road_network = RoadNetwork(scenario.lanelet_network, self.config.get("road_network_param"))
-        ego_vehicle = world.vehicle_by_id(32)
+        # ego_vehicle = world.vehicle_by_id(30)
+        # target_vehicle = world.vehicle_by_id(32)
+        ego_vehicle = world.vehicle_by_id(31)
         target_vehicle = world.vehicle_by_id(30)
+        rob = list()
         for time in range(min(ego_vehicle.end_time, target_vehicle.end_time) + 1):
+            print('---------------------------------')
+            print(time)
             pred = PredInIntersectionConflictArea(self.config)
             sol_monitor_1 = pred.evaluate_boolean(world, time, [ego_vehicle.id, target_vehicle.id])
             print(sol_monitor_1)
 
             sol_monitor_2 = pred.evaluate_robustness(world, time, [ego_vehicle.id, target_vehicle.id])
+            rob.append(sol_monitor_2)
             print(sol_monitor_2)
+        fig = plt.figure()
+        ax = fig.gca()
+        ax.plot(range(min(ego_vehicle.end_time, target_vehicle.end_time) + 1), rob, 'b-')
+        for time in range(min(ego_vehicle.end_time, target_vehicle.end_time) + 1):
+            if rob[time] <= 0:
+                ax.plot(time, rob[time], "rx")
+            else:
+                ax.plot(time, rob[time], "g.")
+        ax.plot([24, 24], [-1, 1], 'black')
+        ax.set_ylim([-1, 1])
+        ax.grid(True)
+        ax.set_title('in_intersection_conflict_area__a1_a0')
+        plt.show()
 
     def testOnLaneletWithTypeIntersection(self):
         scenario, _ = CommonRoadFileReader(
