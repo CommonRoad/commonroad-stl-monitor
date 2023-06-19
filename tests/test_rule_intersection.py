0ec71b5459a14924d99bd1e703b675a1f30da2cc
Luis Gressenbuch <luis.gressenbuch@tum.de>
2023-06-19 16:20:51 +0200

    Cleanup test

--- tests/test_rule_intersection.py
+++ tests/test_rule_intersection.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import unittest
 from pathlib import Path
@@ -11,14 +10,8 @@ from crmonitor.common.world import World
 from crmonitor.evaluation.evaluation import RuleEvaluator
 from crmonitor.monitor.rule import parse_rule
 
-logging.basicConfig(
-    format="%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-    datefmt="%Y-%m-%d:%H:%M:%S",
-    level=logging.INFO,
-)
 
-
-class RuleTest(unittest.TestCase):
+class TestIntersectionRules(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         root_path = Path(__file__).parents[1] / "crmonitor"
@@ -60,6 +53,35 @@ class RuleTest(unittest.TestCase):
             bool_value = rule_robustness >= 0.0
             self.assertEqual(
                 exp_violation, np.all(bool_value), f"Test failed for ego_id={ego_id}"
+            )
+
+    def test_stop_line(self):
+        scenario_file = (
+            self.scenario_root_path / "test_intersection/DEU_test_stop_line.xml"
+        )
+        scenario, planning_problem_set = CommonRoadFileReader(str(scenario_file)).open(
+            lanelet_assignment=True
+        )
+        world = World.create_from_scenario(scenario)
+
+        expected = {
+            1000: True,  # Ignored stop line
+            1001: False,  # Waited forever
+            1002: False,  # Waited long enough
+            1003: True,  # Waited too far away (and not long enough)
+        }
+
+        for ego in scenario.dynamic_obstacles:
+            ego_vehicle = world.vehicle_by_id(ego.obstacle_id)
+            rule_eval = RuleEvaluator.create_from_config(
+                world, ego_vehicle, "R_IN1_past"
+            )
+            values = rule_eval.evaluate()
+            violated = np.any(values < 0.0)
+            self.assertEqual(
+                expected[ego.obstacle_id],
+                violated,
+                f"Test failed for ID={ego.obstacle_id}, {expected[ego.obstacle_id]} != {violated}",
             )
 
 
