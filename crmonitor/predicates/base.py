b13f1cdb13d8c732e9331727bd3d4fd7565bfc15
yuanfei <yuanfei.lin@tum.de>
2023-07-03 15:20:56 +0200

    improve import

--- crmonitor/predicates/base.py
+++ crmonitor/predicates/base.py
@@ -9,7 +9,8 @@ from ruamel.yaml.comments import CommentedMap
 
 from crmonitor.common.world import World, Vehicle
 from crmonitor.predicates.utils import (
-    distance_veh_center_to_lane_boundaries, bool_to_num
+    distance_veh_center_to_lane_boundaries,
+    bool_to_num
 )
 from commonroad_mpr.learning import PredicateEvaluatorML as PEML
 
