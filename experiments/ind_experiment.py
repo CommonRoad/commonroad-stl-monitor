bbf6b4c46702d0d9173a8463a751996ea793d8a3
Zekun Xing <zekun.xing@tum.de>
2023-09-09 17:23:13 +0200

    ind evaluation

--- experiments/ind_experiment.py
+++ experiments/ind_experiment.py
@@ -10,12 +10,12 @@ def generate_learning_data(scenario_path, filename):
         save_filepath=root_path,
         max_scenario_number=400,
     )
-    # generator.generate_data_parallel()
-    evaluator.evaluation()
+    evaluator.evaluation_parallel()
+    # evaluator.evaluation()
 
 
 if __name__ == "__main__":
-    # scenario_path = "/home/ge23lac/scenarios/inD_1_2/"
-    scenario_path = "/home/zekun/MA/scenarios/AAH1_1"
+    scenario_path = "/home/ge23lac/scenarios/AAH1_test/"
+    # scenario_path = "/home/zekun/MA/scenarios/AAH1_1"
     filename = "learning_data.csv"
     generate_learning_data(scenario_path, filename)
