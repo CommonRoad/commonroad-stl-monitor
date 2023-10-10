b05605bb741fdd6b1342329ff0eb97170acb0202
Zekun Xing <zekun.xing@tum.de>
2023-10-10 13:37:31 +0200

    fix state list cr

--- crmonitor/predicates/utils.py
+++ crmonitor/predicates/utils.py
@@ -155,13 +155,23 @@ def distance_veh_center_to_lane_boundaries(
     """
     Distance of the vehicle center to the boundaries of the lane
     """
-    veh_position = vehicle_i.state_list_cr[time_step].position
-    dis_to_left = -lane.clcs_left.convert_to_curvilinear_coords(
-        veh_position[0], veh_position[1]
-    )[1]
-    dis_to_right = lane.clcs_right.convert_to_curvilinear_coords(
-        veh_position[0], veh_position[1]
-    )[1]
+    veh_position = vehicle_i.states_cr[time_step].position
+    try:
+        dis_to_left = -lane.clcs_left.convert_to_curvilinear_coords(
+            veh_position[0], veh_position[1]
+        )[1]
+    except:
+        dis_to_left = -lane.clcs_left_large_step.convert_to_curvilinear_coords(
+            veh_position[0], veh_position[1]
+        )[1]
+    try:
+        dis_to_right = lane.clcs_right.convert_to_curvilinear_coords(
+            veh_position[0], veh_position[1]
+        )[1]
+    except:
+        dis_to_right = lane.clcs_right_large_step.convert_to_curvilinear_coords(
+            veh_position[0], veh_position[1]
+        )[1]
     return dis_to_left, dis_to_right
 
 
