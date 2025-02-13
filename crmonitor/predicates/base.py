af7166bd05879f482927e69d146710c966878287
Jan-Niklas Weghorn <jrester379@gmail.com>
2025-02-13 09:44:26 +0100

    prototype for consoldiated evaluation of predicates

--- crmonitor/predicates/base.py
+++ crmonitor/predicates/base.py
@@ -1,49 +1,19 @@
 import abc
+import copy
 import logging
 import warnings
 from typing import Callable, Dict, List, Tuple
 
 from commonroad.visualization.renderer import IRenderer
 from commonroad_mpr.common.observation import World as WorldMPR
-from commonroad_mpr.learning import FeatureExtrator
-from commonroad_mpr.learning import PredicateEvaluatorML as PEML
-from commonroad_mpr.mpr import ModelPredictiveRobustness
+from commonroad_mpr.prediction.ego_sampling import StateBasedSampling
+from commonroad_mpr.utils.configuration_builder import ConfigurationBuilder as MprCfg
 from ruamel.yaml.comments import CommentedMap
 
 from crmonitor.common.world import World
 from crmonitor.predicates.scaling import RobustnessScaler
 
 logger = logging.getLogger(__name__)
-
-
-def _map_crmonitor_predicate_name_to_mpr_predicate_name(
-    predicate_name: str,
-) -> str:
-    """
-    Returns the predicate name to lookup in mpr. Used to fix up predicate names which differ between crmonitor and mpr.
-    """
-    if predicate_name == "rel_brakes_abruptly":
-        return "brakes_abruptly_relative"
-
-    return predicate_name
-
-
-# Those predicates are non-atomic and a composition of other atomic predicates. They do not have a pendant in mpr, and therefore they get special treatment when evaluating with mpr.
-_COMPOSED_PREDICATES = [
-    "preserves_traffic_flow",
-    "slow_leading_vehicle",
-    "in_congestion",
-    "exist_standing_leading_vehicle",
-    "in_slow_moving_traffic",
-]
-
-
-def _should_use_mpr_predicate_for(predicate_name: str) -> bool:
-    """
-    Determines whether a crmonitor predicate should be replaced with its pendant from mpr.
-    This is usefull, to keep non-atomic predicates from crmonitor, and only replace their sub-predicates with predicates from mpr.
-    """
-    return predicate_name not in _COMPOSED_PREDICATES
 
 
 class BasePredicateEvaluator(abc.ABC):
@@ -59,31 +29,7 @@ class BasePredicateEvaluator(abc.ABC):
         scale_rob = config.get("scale_rob", True)
         self._scaler = scaler or RobustnessScaler(scale_rob)
 
-        # peml and mpr are the evalutors for mpr with and without pre-trained models respectivly.
-        self.peml = None
-        self.mpr = None
-        # There might be
-        mpr_predicate_name = _map_crmonitor_predicate_name_to_mpr_predicate_name(
-            self.predicate_name
-        )
-        self._use_mpr_for_evaluation = self.config[
-            "use_mpr"
-        ] and _should_use_mpr_predicate_for(mpr_predicate_name)
-        if self._use_mpr_for_evaluation:
-            try:
-                self.peml = PEML([mpr_predicate_name])
-            except Exception:
-                warnings.warn(
-                    f"Model for gaussian processes for mpr evaluation of predicate '{self.predicate_name}' is not available. Falling back to MPR without gaussian processes for this predicate. This might result in slower evaluation."
-                )
-                try:
-                    self.mpr = ModelPredictiveRobustness(
-                        [mpr_predicate_name], normalize=scale_rob
-                    )
-                except KeyError as e:
-                    raise RuntimeError(
-                        f"The predicate '{mpr_predicate_name}' is not supported by MPR."
-                    ) from e
+        self._use_mpr_for_evaluation = self.config["use_mpr"]
 
     def _scale_speed(self, x):
         return self._scaler.scale_speed(x)
@@ -115,35 +61,68 @@ class BasePredicateEvaluator(abc.ABC):
         """
         Evaluation of model predictive robustness
         """
-        # computation for single predicate
-        vehicles = []
-        for veh_id in vehicle_ids:
-            vehicles.append(world_mpr.vehicle_by_id(veh_id))
+        ego_vehicle_id = vehicle_ids[0]
+        ego_vehicle_mpr = world_mpr.vehicle_by_id(ego_vehicle_id)
+        ego_sampler = StateBasedSampling(
+            ego_vehicle_mpr,
+            time_step,
+            MprCfg["common"]["scenario"],
+            **MprCfg["sampling_approach"]["state_based_sampling"][
+                MprCfg["common"]["scenario"]
+            ],
+        )
 
-        if self.peml is not None:
-            robustness, _ = self.peml.evaluate_robustness(
-                world=world_mpr, vehicles=vehicles, time_step=time_step
-            )
-            return robustness[0]
-        elif self.mpr is not None:
-            robustness = self.mpr.evaluate(
-                world=world_mpr, vehicles=vehicles, time_step=time_step
-            )
-            # Each mpr evaluator is always configured for exactly one predicate.
-            # As the output predicate names might be different then the input predicate names (TOOD: fix in commonroad-mpr), we simply use the single key as to retrive the robustenss value.
-            mpr_predicate_name = list(robustness.keys())[0]
-            return robustness[mpr_predicate_name]["robustness"]
-        else:
-            # Should be unreachable, because __init__ should ensure that the evaluators are available if we are evaluating with mpr.
-            raise RuntimeError(
-                f"Cannot evaluate model predictive robutness of predicate '{self.predicate_name}', as no evaluator is available. This is a bug."
-            )
+        orig_ego_vehicle = world.vehicle_by_id(ego_vehicle_id)
+        ego_vehicle = copy.deepcopy(orig_ego_vehicle)
+        ego_vehicle.signal_series[time_step] = None
+
+        world.remove_vehicle(orig_ego_vehicle)
+        world.add_vehicle(ego_vehicle)
+
+        count_valid = 0
+        count_true = 0
+        count_error = 0
+        for ego_future_state_mpr in ego_sampler.sample():
+            try:
+                ego_future_state = ego_future_state_mpr.convert_to_commonroad_state()
+                ego_vehicle.states_cr[time_step] = ego_future_state
+                ego_loc_shape = ego_vehicle.shape.rotate_translate_local(
+                    ego_future_state.position, ego_future_state.orientation
+                )
+                lanelet_assignment = (
+                    world.road_network.lanelet_network.find_lanelet_by_shape(
+                        ego_loc_shape
+                    )
+                )
+                if len(lanelet_assignment) == 0:
+                    count_error += 1
+                    continue
+                ego_vehicle.lanelet_assignment[time_step] = lanelet_assignment
+
+                satisfied = self.evaluate_boolean(world, time_step, vehicle_ids)
+                if satisfied:
+                    count_true += 1
+                count_valid += 1
+            except Exception as e:
+                raise e
+                count_error += 1
+
+        world.remove_vehicle(ego_vehicle)
+        world.add_vehicle(orig_ego_vehicle)
+
+        satisfied = self.evaluate_boolean(world, time_step, vehicle_ids)
+        probability = count_true / (count_valid + MprCfg["robustness"]["eps"])
+        robustness = probability if satisfied else -(1 - probability)
+
+        return robustness
 
     def gradient_mpr(self):
         """
         Computes the gradient of the MPR w.r.t. the input values
         """
         default = [0.0] * 35
+        # TODO: reenable
+        return default
         if not self._use_mpr_for_evaluation:
             # TODO: If the user tries to extract the gradient for a comosed/exempted predicate, should it just be skipped?
             warnings.warn(
