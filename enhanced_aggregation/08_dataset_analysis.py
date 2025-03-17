81f6305a1424991e3f0409ec74dfdeef08a490ec
Florian Finkeldei <florian.finkeldei@tum.de>
2025-03-17 15:20:48 +0100

    minor changes.

--- enhanced_aggregation/08_dataset_analysis.py
+++ enhanced_aggregation/08_dataset_analysis.py
@@ -10,7 +10,7 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-learning_data_path = Path(__file__).parent.parent / "output" / "learning_data" / "learning_data_yuanfei.csv"
+learning_data_path = Path(__file__).parent.parent / "output" / "learning_data" / "learning_data.csv"
 
 selected_predicates = yuanfei_predicates
 data_loader = DataLoader.create_from_file(learning_data_path)
@@ -22,31 +22,32 @@ for predicate in selected_predicates:
 
 # check velocity stuff
 velocity_results = {}
-velocities = {
+velocities = {  # km/h
     "has_slow_moving_velocity": 30,
     "velocity_below_20": 20,
     "velocity_below_15": 15,
-    "has_queue_velocity": 10.8,  # 60 according to Maierhofer paper # TODO fix
+    "has_queue_velocity": 60,
     "has_congestion_velocity": 10,
     "velocity_below_5": 5,
     "velocity_below_2": 2,
-    "in_standstill": 0.01,
-    "reverses": -0.01,
+    "in_standstill": 0.036,
+    "reverses": -0.036,
 }
 for predicate in [
     "reverses",
+    "makes_u_turn",
     "in_standstill",
-    # "velocity_below_2",
-    # "velocity_below_5",
+    "velocity_below_2",
+    "velocity_below_5",
     "has_congestion_velocity",
     "has_queue_velocity",
-    # "velocity_below_15",
-    # "velocity_below_20",
+    "velocity_below_15",
+    "velocity_below_20",
     "has_slow_moving_velocity"
 ]:
     data = data_loader.data[("predicates", predicate, "bool")]
     velocity_results[predicate] = sum([1 for x in data if x==True]) / len(data)
-
-#  data_loader.data[[("inputs", "ego", "velocity"), ("predicates", "reverses", "bool")]]
+    # data = data_loader.data[("predicates", predicate, "robustness")]
+    # velocity_results[predicate] =  np.mean([x for x in data if x > 0])
 
 print(velocity_results)
