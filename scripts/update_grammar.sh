0b7b643708ab4f6c4ef1b0d5d2e501ced4e3b089
Jan-Niklas Weghorn <jrester379@gmail.com>
2025-09-05 10:30:41 +0200

    fix(misc): grammar generation does not create new lexer

--- scripts/update_grammar.sh
+++ scripts/update_grammar.sh
@@ -2,11 +2,9 @@
 echo "Updating external grammar..."
 cd external/rtamt/rtamt/antlr/grammar/tl
 echo "LTL"
-antlr4 -visitor -no-listener -o ../../parser/ltl/ -Dlanguage=Python3 LtlLexer.g4
-antlr4 -visitor -no-listener -o ../../parser/ltl/ -Dlanguage=Python3 -lib ../../parser/ltl LtlParser.g4
+antlr4 -visitor -no-listener -o ../../parser/ltl/ -Dlanguage=Python3 -lib ../../parser/ltl LtlLexer.g4 LtlParser.g4
 echo "STL"
-antlr4 -visitor -no-listener -o ../../parser/stl/ -Dlanguage=Python3 StlLexer.g4
-antlr4 -visitor -no-listener -o ../../parser/stl/ -Dlanguage=Python3 -lib ../../parser/stl StlParser.g4
+antlr4 -visitor -no-listener -o ../../parser/stl/ -Dlanguage=Python3 -lib ../../parser/stl LtlLexer.g4 StlParser.g4
 
 echo "Updating custom grammar..."
 cd ../../../../../../crmonitor/rule/fastl/
